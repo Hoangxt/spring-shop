591c30d1674f572323e7cce183800ae95a105000
Dinh Huy Hoang <80768175+Hoangxt@users.noreply.github.com>
2024-08-07 17:00:47 +0700

    add fake data to products

--- src/main/java/com/project/shopapp/controllers/ProductController.java
+++ src/main/java/com/project/shopapp/controllers/ProductController.java
@@ -1,16 +1,26 @@
 package com.project.shopapp.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.javafaker.Faker;
 import com.project.shopapp.components.LocalizationUtils;
 import com.project.shopapp.dto.ProductDTO;
 import com.project.shopapp.dto.ProductImageDTO;
 import com.project.shopapp.models.Product;
 import com.project.shopapp.models.ProductImage;
 import com.project.shopapp.responses.ResponseObject;
+import com.project.shopapp.responses.product.ProductListResponse;
+import com.project.shopapp.responses.product.ProductResponse;
 import com.project.shopapp.services.product.IProductService;
 import com.project.shopapp.utils.FileUtils;
 import com.project.shopapp.utils.MessageKeys;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.UrlResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,13 +29,17 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("${api.prefix}/products")
 @RequiredArgsConstructor
 public class ProductController {
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
     private final IProductService productService;
     private final LocalizationUtils localizationUtils;
 
@@ -113,6 +127,152 @@ public class ProductController {
                 .message("Upload image successfully")
                 .status(HttpStatus.CREATED)
                 .data(productImages)
+                .build());
+    }
+
+    // xem anh cua san pham da upload
+    @GetMapping("/images/{imageName}")
+    public ResponseEntity<?> viewImage(@PathVariable String imageName){
+        try {
+            java.nio.file.Path imagePath = Paths.get("uploads/"+imageName);
+            UrlResource resource = new UrlResource(imagePath.toUri());
+
+            if (resource.exists()) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_JPEG)
+                        .body(resource);
+            } else {
+                logger.info(imageName + " not found");
+                return ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_JPEG)
+                        .body(new UrlResource(Paths.get("uploads/notfound.jpeg").toUri()));
+                //return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            logger.error("Error occurred while retrieving image: " + e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // get all products
+//    @GetMapping("")
+//    public ResponseEntity<ResponseObject> getProducts(
+//            @RequestParam(defaultValue = "") String keyword,
+//            @RequestParam(defaultValue = "0", name = "category_id") Long categoryId,
+//            @RequestParam(defaultValue = "0") int page,
+//            @RequestParam(defaultValue = "10") int limit
+//    ) throws JsonProcessingException {
+//        int totalPages = 0;
+//        //productRedisService.clear();
+//        // Tạo Pageable từ thông tin trang và giới hạn
+//        PageRequest pageRequest = PageRequest.of(
+//                page, limit,
+//                //Sort.by("createdAt").descending()
+//                Sort.by("id").ascending()
+//        );
+//        logger.info(String.format("keyword = %s, category_id = %d, page = %d, limit = %d",
+//                keyword, categoryId, page, limit));
+//        List<ProductResponse> productResponses = productRedisService
+//                .getAllProducts(keyword, categoryId, pageRequest);
+//        if (productResponses!=null && !productResponses.isEmpty()) {
+//            totalPages = productResponses.get(0).getTotalPages();
+//        }
+//        if(productResponses == null) {
+//            Page<ProductResponse> productPage = productService
+//                    .getAllProducts(keyword, categoryId, pageRequest);
+//            // Lấy tổng số trang
+//            totalPages = productPage.getTotalPages();
+//            productResponses = productPage.getContent();
+//            // Bổ sung totalPages vào các đối tượng ProductResponse
+//            for (ProductResponse product : productResponses) {
+//                product.setTotalPages(totalPages);
+//            }
+//            productRedisService.saveAllProducts(
+//                    productResponses,
+//                    keyword,
+//                    categoryId,
+//                    pageRequest
+//            );
+//        }
+//        ProductListResponse productListResponse = ProductListResponse
+//                .builder()
+//                .products(productResponses)
+//                .totalPages(totalPages)
+//                .build();
+//        return ResponseEntity.ok().body(ResponseObject.builder()
+//                .message("Get products successfully")
+//                .status(HttpStatus.OK)
+//                .data(productListResponse)
+//                .build());
+//    }
+
+    // get product by id
+    //http://localhost:8088/api/v1/products/6
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseObject> getProductById(
+            @PathVariable("id") Long productId
+    ) throws Exception {
+        Product existingProduct = productService.getProductById(productId);
+        return ResponseEntity.ok(ResponseObject.builder()
+                .data(ProductResponse.fromProduct(existingProduct))
+                .message("Get detail product successfully")
+                .status(HttpStatus.OK)
+                .build());
+
+    }
+
+    // get product by ids
+    @GetMapping("/by-ids")
+    public ResponseEntity<ResponseObject> getProductsByIds(@RequestParam("ids") String ids) {
+        //eg: 1,3,5,7
+        // Tách chuỗi ids thành một mảng các số nguyên
+        List<Long> productIds = Arrays.stream(ids.split(","))
+                .map(Long::parseLong)
+                .collect(Collectors.toList());
+        List<Product> products = productService.findProductsByIds(productIds);
+        return ResponseEntity.ok(ResponseObject.builder()
+                .data(products.stream().map(ProductResponse::fromProduct).toList())
+                .message("Get products successfully")
+                .status(HttpStatus.OK)
+                .build()
+        );
+    }
+
+    // delete product by id
+    @DeleteMapping("/{id}")
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+//    @Operation(security = { @SecurityRequirement(name = "bearer-key") })
+    public ResponseEntity<ResponseObject> deleteProduct(@PathVariable long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.ok(ResponseObject.builder()
+                .data(null)
+                .message(String.format("Product with id = %d deleted successfully", id))
+                .status(HttpStatus.OK)
+                .build());
+    }
+
+    // fake data
+    @PostMapping("/generateFakeProducts")
+    private ResponseEntity<ResponseObject> generateFakeProducts() throws Exception {
+        Faker faker = new Faker();
+        for (int i = 0; i < 20; i++) {
+            String productName = faker.commerce().productName();
+            if(productService.existsByName(productName)) {
+                continue;
+            }
+            ProductDTO productDTO = ProductDTO.builder()
+                    .name(productName)
+                    .price((float)faker.number().numberBetween(10, 90_000_000))
+                    .description(faker.lorem().sentence())
+                    .thumbnail("")
+                    .categoryId((long)faker.number().numberBetween(1, 3))
+                    .build();
+            productService.createProduct(productDTO);
+        }
+        return ResponseEntity.ok(ResponseObject.builder()
+                .message("Insert fake products successfully")
+                .data(null)
+                .status(HttpStatus.OK)
                 .build());
     }
 }
