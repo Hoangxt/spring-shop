f4b3f5d3d59c0b3603e0938759cb022cff5ace06
Dinh Huy Hoang <80768175+Hoangxt@users.noreply.github.com>
2024-08-07 19:45:26 +0700

    order service (not done)

---
+++ src/main/java/com/project/shopapp/services/orders/OrderService.java
@@ -0,0 +1,134 @@
+package com.project.shopapp.services.orders;
+
+import com.project.shopapp.dto.OrderDTO;
+import com.project.shopapp.dto.CartItemDTO;
+import com.project.shopapp.exceptions.DataNotFoundException;
+import com.project.shopapp.models.*;
+import com.project.shopapp.repositories.OrderDetailRepository;
+import com.project.shopapp.repositories.OrderRepository;
+import com.project.shopapp.repositories.ProductRepository;
+import com.project.shopapp.repositories.UserRepository;
+import com.project.shopapp.responses.order.OrderResponse;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService implements IOrderService{
+    // inject các repository
+    private final UserRepository userRepository;
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+    private final OrderDetailRepository orderDetailRepository;
+
+    // create config cho model mapper trước để tránh lỗi
+    private final ModelMapper modelMapper;
+    @Override
+    @Transactional
+    public Order createOrder(OrderDTO orderDTO) throws Exception {
+        //tìm xem user'id có tồn tại ko
+        User user = userRepository
+                .findById(orderDTO.getUserId())
+                .orElseThrow(() -> new DataNotFoundException("Cannot find user with id: "+orderDTO.getUserId()));
+        //convert orderDTO => Order
+        //dùng thư viện Model Mapper
+        // Tạo một luồng bảng ánh xạ riêng để kiểm soát việc ánh xạ
+        // Bỏ qua trường id (OrderDTO ko co id)
+        modelMapper.typeMap(OrderDTO.class, Order.class)
+                .addMappings(mapper -> mapper.skip(Order::setId));
+        // Cập nhật các trường của đơn hàng từ orderDTO
+        Order order = new Order();
+        modelMapper.map(orderDTO, order);
+        order.setUser(user);
+        order.setOrderDate(LocalDateTime.now());//lấy thời điểm hiện tại
+        order.setStatus(OrderStatus.PENDING);
+        //Kiểm tra shipping date phải >= ngày hôm nay
+        LocalDate shippingDate = orderDTO.getShippingDate() == null
+                ? LocalDate.now() : orderDTO.getShippingDate();
+        if (shippingDate.isBefore(LocalDate.now())) {
+            throw new DataNotFoundException("Date must be at least today !");
+        }
+        order.setShippingDate(shippingDate);
+        order.setActive(true);//đoạn này nên set sẵn trong sql
+        //EAV-Entity-Attribute-Value model
+        order.setTotalMoney(orderDTO.getTotalMoney());
+        // Tạo danh sách các đối tượng OrderDetail từ cartItems
+        List<OrderDetail> orderDetails = new ArrayList<>();
+        for (CartItemDTO cartItemDTO : orderDTO.getCartItems()) {
+            // Tạo một đối tượng OrderDetail từ CartItemDTO
+            OrderDetail orderDetail = new OrderDetail();
+            orderDetail.setOrder(order);
+
+            // Lấy thông tin sản phẩm từ cartItemDTO
+            Long productId = cartItemDTO.getProductId();
+            int quantity = cartItemDTO.getQuantity();
+
+            // Tìm thông tin sản phẩm từ cơ sở dữ liệu (hoặc sử dụng cache nếu cần)
+            Product product = productRepository.findById(productId)
+                    .orElseThrow(() -> new DataNotFoundException("Product not found with id: " + productId));
+
+            // Đặt thông tin cho OrderDetail
+            orderDetail.setProduct(product);
+            orderDetail.setNumberOfProducts(quantity);
+            // Các trường khác của OrderDetail nếu cần
+            orderDetail.setPrice(product.getPrice());
+
+            // Thêm OrderDetail vào danh sách
+            orderDetails.add(orderDetail);
+        }
+
+        //coupon
+//        String couponCode = orderDTO.getCouponCode();
+//        if (!couponCode.isEmpty()) {
+//            Coupon coupon = couponRepository.findByCode(couponCode)
+//                    .orElseThrow(() -> new IllegalArgumentException("Coupon not found"));
+//
+//            if (!coupon.isActive()) {
+//                throw new IllegalArgumentException("Coupon is not active");
+//            }
+//
+//            order.setCoupon(coupon);
+//        } else {
+//            order.setCoupon(null);
+//        }
+
+        // Lưu danh sách OrderDetail vào cơ sở dữ liệu
+        orderDetailRepository.saveAll(orderDetails);
+        orderRepository.save(order);
+        return order;
+    }
+
+    @Override
+    public Order getOrderById(Long orderId) {
+        return null;
+    }
+
+    @Override
+    public Order updateOrder(Long id, OrderDTO orderDTO) throws DataNotFoundException {
+        return null;
+    }
+
+    @Override
+    public void deleteOrder(Long orderId) {
+
+    }
+
+    @Override
+    public List<OrderResponse> findByUserId(Long userId) {
+        return null;
+    }
+
+    @Override
+    public Page<Order> getOrdersByKeyword(String keyword, Pageable pageable) {
+        return null;
+    }
+}
